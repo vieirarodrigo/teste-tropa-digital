c8447d564f347134bb0e6d321072ef84e7262571
vieirarodrigo <vieirarodrigohenrique@gmail.com>
2025-06-05 16:17:39 -0300

    Configurado o `styled components`

---
+++ app/components/MyStyleComponent.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 2em;
+`;
+
+export default function MyStyledComponent() {
+  return (
+    <Title>Em construção</Title>
+  );
+}
